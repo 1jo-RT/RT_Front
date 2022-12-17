36c9aa90c2c852105560bcf84ace59ce3b536830
minseung-gang <strause1@nate.com>
2022-12-17 14:11:21 +0900

    Docs: 텍스트 수정

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,10 +3,3 @@ import React from "react";
 export default function Home() {
   return <div>Home</div>;
 }
-import React from 'react'
-
-export default function Home() {
-  return (
-    <div>Home</div>
-  )
-}
