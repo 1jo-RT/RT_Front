d61ccedf4c2e1b8a31db52d014fd1a2e0a82a5d0
minseung-gang <strause1@nate.com>
2022-12-21 16:24:09 +0900

    FEAT: 디테일 페이지 경로 설정 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from '../components/pages/Home';
+import Home from '../components/layout/Home';
 import Login from '../components/pages/Login';
+import Kakao from '../components/kakao/Kakao';
+import Detail from '../components/pages/Detail';
 
 export default function Router() {
 
     return (
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/boards/:id" element={<Detail />} />
+            <Route path="/api/user/login" element={<Login />}/>
+            <Route path="/api/user/kakao/callback" element={<Kakao />}></Route>
           </Routes>
         </BrowserRouter>
       );
