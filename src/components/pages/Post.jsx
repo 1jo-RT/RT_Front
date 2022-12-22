c3112eb36767281d53e1c0650a5af5ecffe8e3a1
minseung-gang <strause1@nate.com>
2022-12-22 22:20:03 +0900

    DOCS: 회원가입 문서 수정

--- src/components/pages/Post.jsx
+++ src/components/pages/Post.jsx
@@ -1,68 +1,100 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
 import styled from "styled-components";
 import { __createPost } from "../../redux/modules/postSlice";
 
 function Post() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [post, setPost] = useState({ title: "", content: "" });
-  const [image, setImage] = useState("");
+  const [post, setPost] = useState({
+    title: "",
+    content: "",
+  });
   const { title, content } = post;
+  // const [image, setImage] = useState({
+  //   image_file: "",
+  //   preview_URL:
+  //     "https://thumb.mt.co.kr/06/2022/05/2022052719301144973_1.jpg/dims/optimize/",
+  // });
 
   const onChangePostHandler = (e) => {
     const { name, value } = e.target;
     setPost({ ...post, [name]: value });
-    console.log(post);
+    console.log({ title, content });
   };
 
-  const uploadImage = (e) => {
-    console.log(e.target.files);
-    e.preventDefault();
-    setImage(URL.createObjectURL(e.target.files[0]));
-  };
+  //이미지 업로드
+  // const uploadImage = (e) => {
+  //   console.log(e.target.files);
+  //   e.preventDefault();
+  //   const preview_URL = URL.createObjectURL(e.target.files[0]); //임시 blob주소를 만들어준다.
 
-  const onSubmitPostHandler = (e) => {
-    e.preventDefault();
-    dispatch(__createPost({ title, content }));
+  //   setImage(() => ({
+  //     image_file: e.target.files[0],
+  //     preview_URL: preview_URL,
+  //   }));
+  //   setPost({ ...post, image: image.preview_URL });
+  // };
+
+  // useEffect(() => {
+  //   return () => {
+  //     URL.revokeObjectURL(image.preview_URL); // 삭제해서 데이터 누수를 줄일 수 있다.
+  //   };
+  // }, [image.preview_URL]);
+
+  const onSubmitPostHandler = () => {
+    // const formData = new FormData();
+    // // const json = JSON.stringify(image);
+    // // const blob = new Blob([json], { type: "application/json" });
+    // formData.append("image", image.image_file); //append의 첫번째 인자에는 name, 두번째는 value
+    // formData.append(
+    //   "post",
+    //   post
+    //   // new Blob([JSON.stringify(post)], "type:application/json") //type은 axios쓰면 자동으로.
+    // );
+    dispatch(__createPost(post));
     alert("게시글 등록 완료");
+    navigate("/api/boards");
   };
 
   return (
     <>
-      <form onSubmit={onSubmitPostHandler}>
-        <Layout>
-          <ImageContainer>
-            <input
-              type="file"
-              name="file"
-              accept="image/*"
-              onChange={uploadImage}
-            />
-            <button>submit</button>
-          </ImageContainer>
-          <InputContainer>
-            <TitleInput
-              type="text"
-              name="title" // 제출된 후 서버에서 폼데이터 참조..? 자바스크립트에서 요소 참조
-              value={post.title || ""}
-              placeholder="제목을 입력해주세요"
-              onChange={onChangePostHandler}
-            ></TitleInput>
-            <ContentInput
-              type="text"
-              name="content"
-              value={post.content || ""}
-              placeholder="내용을 입력하세요"
-              onChange={onChangePostHandler}
-            ></ContentInput>
-            <div>
-              <PostButton>이미지 추가</PostButton>
-              <PostButton>업로드</PostButton>
+      <Layout>
+        <ImageContainer>
+          {/* <input
+            type="file"
+            name="image_file"
+            accept="image/*"
+            onChange={uploadImage}
+          />
+          <PostImg src={image.preview_URL} alt="" />
+          <button>submit</button> */}
+        </ImageContainer>
+        <InputContainer>
+          <TitleInput
+            type="text"
+            name="title" // 제출된 후 서버에서 폼데이터 참조..? 자바스크립트에서 요소 참조
+            value={title || ""}
+            placeholder="제목을 입력해주세요"
+            onChange={onChangePostHandler}
+          ></TitleInput>
+          <ContentInput
+            type="text"
+            name="content"
+            value={content || ""}
+            placeholder="내용을 입력하세요"
+            onChange={onChangePostHandler}
+          ></ContentInput>
+          <div>
+            {/* <PostButton>이미지 추가</PostButton> */}
+            <PostButton onClick={onSubmitPostHandler}>업로드</PostButton>
+            <Link to="/">
               <PostButton>닫기</PostButton>
-            </div>
-          </InputContainer>
-        </Layout>
-      </form>
+            </Link>
+          </div>
+        </InputContainer>
+      </Layout>
     </>
   );
 }
@@ -80,11 +112,17 @@ const Layout = styled.div`
   transform: translate(-50%, -40%);
   border: 3px solid blue;
   display: flex;
+  overflow: hidden;
 `;
 const ImageContainer = styled.div`
   width: 50%;
   border: 3px solid yellow;
 `;
+// const PostImg = styled.img`
+//   width: 100%;
+//   height: 100%;
+//   object-fit: cover;
+// `;
 const InputContainer = styled.div`
   width: 50%;
   border: 3px solid green;
