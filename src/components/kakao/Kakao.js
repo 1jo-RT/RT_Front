8a972b036705a673c1603c9efb3c33d5010515c9
minseung-gang <strause1@nate.com>
2022-12-22 22:18:30 +0900

    FEAT:rediretURL 수정 및 로컬스토리지 설정 추가

--- src/components/kakao/Kakao.js
+++ src/components/kakao/Kakao.js
@@ -9,10 +9,8 @@ const Kakao = (props) => {
   // 인가코드
   let code = new URL(window.location.href).searchParams.get("code");
   console.log('code:',code);
-  
- useEffect(() => {
-     dispatch(kakaoLogin(code));
-  }, []);
+  dispatch(kakaoLogin(code));
+ 
 
   return <div>ddd</div>;
 };
