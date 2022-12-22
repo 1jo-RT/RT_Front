7fe9356afe59a97603aeee36b019b9d8c459780b
minseung-gang <strause1@nate.com>
2022-12-22 22:17:49 +0900

    DOCS: 라우터 회원가입 및 글쓰기 페이지 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -4,6 +4,8 @@ import Home from '../components/layout/Home';
 import Login from '../components/pages/Login';
 import Kakao from '../components/kakao/Kakao';
 import Detail from '../components/pages/Detail';
+import SignUp from '../components/pages/SignUp';
+import Post from '../components/pages/Post';
 
 export default function Router() {
 
@@ -11,6 +13,8 @@ export default function Router() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/api/user" element={<SignUp />} />
+            <Route path="/api/boards/newboard" element={<Post />} />
             <Route path="/boards/:id" element={<Detail />} />
             <Route path="/api/user/login" element={<Login />}/>
             <Route path="/api/user/kakao/callback" element={<Kakao />}></Route>
